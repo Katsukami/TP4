c1bf1496fc8b72b4d8a25333d43e7d450ec38ee0
Katsukami <112531044+Katsukami@users.noreply.github.com>
2025-03-16 20:30:58 -0400

    Adding correctly the file

--- myscript.py
+++ myscript.py
@@ -1,3 +1,3 @@
 import os
 os.system("git bisect start 6b80294bb0ce203946adbb699023e1634e3b5289 6a82add9801de043a8ffc51b5a02d695238a3c01")
-os.system("git bisect run python myscript.py test")
+os.system("git bisect run python manage.py test")
